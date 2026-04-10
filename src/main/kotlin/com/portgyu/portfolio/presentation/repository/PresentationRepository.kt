3b4e3f9531af4d3a19a12fc76c7ab3d41a60a17b
gyuminwoo <ug034516@gmail.com>
2026-04-10 19:47:05 +0900

    리포지토리 개발

--- src/main/kotlin/com/portgyu/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/portgyu/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,7 +1,44 @@
 package com.portgyu.portfolio.presentation.repository
 
+import com.portgyu.portfolio.domain.entity.Achievement
+import com.portgyu.portfolio.domain.entity.Experience
+import com.portgyu.portfolio.domain.entity.Introduction
+import com.portgyu.portfolio.domain.entity.Link
+import com.portgyu.portfolio.domain.entity.Project
+import com.portgyu.portfolio.domain.entity.Skill
+import com.portgyu.portfolio.domain.repository.AchievementRepository
+import com.portgyu.portfolio.domain.repository.ExperienceRepository
+import com.portgyu.portfolio.domain.repository.IntroductionRepository
+import com.portgyu.portfolio.domain.repository.LinkRepository
+import com.portgyu.portfolio.domain.repository.ProjectRepository
+import com.portgyu.portfolio.domain.repository.SkillRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository {
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val experienceRepository: ExperienceRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val projectRepository: ProjectRepository,
+    private val skillRepository: SkillRepository
+) {
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+    fun getActiveExperiences(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+    fun getActiveIntroductions(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+    fun getActiveLinks(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+    fun getActiveProjects(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+    fun getActiveSkills(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
 }
